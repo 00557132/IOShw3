7059612f0f7f430a8c8a3d2865282d35e45692b2
YiChunWang <partypie3091@gmail.com>
2019-11-11 02:03:14 +0800

    IOS_Hw3_Binding

--- IOShw3/ContentView.swift
+++ IOShw3/ContentView.swift
@@ -1,6 +1,6 @@
 //
 //  ContentView.swift
-//  IOShw3
+//  IOS_Hw3
 //
 //  Created by blaire on 2019/11/10.
 //  Copyright © 2019 AppCoda. All rights reserved.
@@ -9,8 +9,40 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showimg = false
+    @State private var selectIndex = 0;
     var body: some View {
-        Text("Hello World")
+        NavigationView{
+            VStack{
+                Form{
+                    Text(games[selectIndex].name)
+                        .font(.largeTitle)
+                        .foregroundColor(.red)
+                    
+                    VStack{
+                        if showimg {
+                            NavigationLink(destination: GameDetail(game: games[selectIndex])) {
+                                Image(games[selectIndex].generation)
+                               .resizable()
+                               .scaledToFill()
+                               .frame(width: 380, height: 420)
+                               .clipped()
+                              .animation(Animation.linear(duration: 1).repeatCount(1, autoreverses: false))
+                            }
+                        } else {
+                        }
+                         Toggle("顯示圖片", isOn: $showimg)
+                    }
+                    
+                    Picker(selection: $selectIndex, label: Text("年份").foregroundColor(.orange)) {
+                      ForEach(0..<games.count) { (index) in
+                            Text(games[index].year)
+                                .foregroundColor(.blue)
+                        }
+                    }.pickerStyle(WheelPickerStyle())
+                }
+            }
+        }
     }
 }
 
